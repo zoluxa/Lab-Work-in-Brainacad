219128323173cc335bb800d00df425d65c8b7981
zoluxa <zoluxa@gmail.com>
2016-04-03 17:38:26 +0300

    Done lab 1.2.3

---
+++ lab-1.2.3/lab-1.2.3/Money.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_1._2._3
+{
+    // 1) declare enumeration CurrencyTypes, values UAH, USD, EU
+
+    enum CurrencyTypes
+    {
+        UAH, USD, EU
+    }
+    class Money
+    {
+        // 2) declare 2 properties Amount, CurrencyType
+        public double Amount { get; set; }
+        public CurrencyTypes CurrencyType { get; set; }
+
+        // 3) declare parameter constructor for properties initialization
+        public Money (double set_Amount, CurrencyTypes set_CurrencyTypes) {
+            this.Amount = set_Amount;
+            this.CurrencyType = set_CurrencyTypes;
+        }
+        // 4) declare overloading of operator + to add 2 objects of Money
+        public static Money operator +(Money value1, Money value2) {
+            Money summ = new Money(value1.Amount + value2.Amount, value1.CurrencyType);
+            return summ;
+        }
+        // 5) declare overloading of operator -- to decrease object of Money by 1
+        public static Money operator --(Money value1)
+        {
+            Money mines = new Money(--value1.Amount, value1.CurrencyType);
+            return mines;
+        }
+        // 6) declare overloading of operator * to increase object of Money 3 times
+        public static Money operator *(Money value1, int incr)
+        {
+            Money res = new Money(value1.Amount *= incr, value1.CurrencyType);
+            return res;
+        }
+        // 7) declare overloading of operator > and < to compare 2 objects of Money
+        public static bool operator >(Money value1, Money value2)
+        {
+            if (value1.Amount > value2.Amount)
+                return true;
+            else
+                return false;
+        }
+        public static bool operator <(Money value1, Money value2)
+        {
+            if (value1.Amount < value2.Amount)
+                return true;
+            else
+                return false;
+        }
+        // 8) declare overloading of operator true and false to check CurrencyType of object
+        public static bool operator true(Money v)
+        {
+            if (v.CurrencyType == CurrencyTypes.EU)
+                return true;
+            else
+                return false;
+        }
+
+        public static bool operator false(Money v)
+        {
+            if (v.CurrencyType == CurrencyTypes.UAH)
+                return false;
+            else
+                return true;
+        }
+
+        // 9) declare overloading of implicit/ explicit conversion  to convert Money to double, string and vice versa
+        public static implicit operator double(Money arr)
+        {
+            double impl = arr.Amount;
+            return impl;
+        }
+
+        public static explicit operator string(Money arr)
+        {
+            string expl = arr.Amount.ToString();
+            return expl;
+        }
+        public static explicit operator Money(double arr)
+        {
+            Money expl = new Money(arr, CurrencyTypes.USD);
+            return expl;
+        }
+
+    }
+}
---
+++ lab-1.2.3/lab-1.2.3/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_1._2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 10) declare 2 objects
+            Money obj1 = new Money(150, CurrencyTypes.UAH);
+            Money obj2 = new Money(200, CurrencyTypes.UAH);
+
+            Console.WriteLine("Money in my bag: {0} and {1}", obj1.Amount, obj2.Amount);
+            // 11) do operations
+            // add 2 objects of Money
+            obj2 += obj1;
+            Console.WriteLine("I have {0} UAH", obj2.Amount);
+            // add 1st object of Money and double
+            
+            obj1.Amount += 10.4;
+            Console.WriteLine("I get payment {0} UAH", obj1.Amount);
+            // decrease 2nd object of Money by 1 
+            --obj2;
+            Console.WriteLine("Comission payment: {0}", obj2.Amount);
+            // increase 1st object of Money
+            obj1 *= 2;
+            Console.WriteLine("Bonus: {0}", obj1.Amount);
+            // compare 2 objects of Money
+            if (obj1>obj2)
+            {
+                Console.WriteLine("Object 1 bigger than Object 2");
+            }
+            else if (obj1 == obj2)
+            {
+                Console.WriteLine("Object 1 have the same money that Object 2");
+            }
+            else
+            {
+                Console.WriteLine("Object 1 smaller than Object 2");
+            }
+            // compare 2nd object of Money and string
+            string comp = "222";
+            Console.WriteLine("Compare Object 2 {0} and string {1}", obj2.Amount, comp);
+            if (String.Equals(obj2.Amount,comp))
+            {
+                Console.WriteLine("They are equal");
+            }
+            else
+            {
+                Console.WriteLine("They are not equal");
+            }
+            // check CurrencyType of every object
+            Console.WriteLine("Currency types of Object 1 - {0} and Object 2 - {1}", obj1.CurrencyType, obj2.CurrencyType);
+            // convert 1st object of Money to string
+            Convert.ToString(obj1);
+            Console.WriteLine("Object 1 converted to string... ", obj1);
+        }
+    }
+}
